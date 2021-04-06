4e0fd277c17334bdeae3114f2f0e9c1dc68285ee
francescomarocchi <francesco.marocchi@gmail.com>
2021-04-06 17:42:46 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { StuffFluffComponent } from './stuff-fluff/stuff-fluff.component';
-import { StuffChildrenComponent } from './stuff-children/stuff-children.component';
+import { AppComponent } from "./app.component";
+import { StuffFluffComponent } from "./stuff-fluff/stuff-fluff.component";
+import { StuffChildrenComponent } from "./stuff-children/stuff-children.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StuffFluffComponent, StuffChildrenComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, StuffFluffComponent, StuffChildrenComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
