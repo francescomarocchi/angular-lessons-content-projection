7b1ea2a7a98c887998491c3ae22d91d6c18d98c4
francescomarocchi <francesco.marocchi@gmail.com>
2021-04-06 17:59:58 +0200

    1 file modified, 3 added, 3 deleted

---
+++ src/app/parent-projector/parent-projector.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-parent-projector",
+  templateUrl: "./parent-projector.component.html",
+  styleUrls: ["./parent-projector.component.css"]
+})
+export class StuffFluffComponent implements OnInit {
+  @Input() showBefore = false;
+  constructor() {}
+
+  ngOnInit() {}
+}
